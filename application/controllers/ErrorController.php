4b139bea422bd3bb8848de087060afb0f13437b8
Sylvareth <dieter.nonneman@hotmail.com>
2013-07-30 23:44:02 +0200

    Addition of custom 404 page

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -19,13 +19,28 @@ class ErrorController extends Zend_Controller_Action
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
                 $priority = Zend_Log::NOTICE;
-                $this->view->message = 'Page not found';
+                $this->view->error_code = $this->getResponse()->getHttpResponseCode();
+                $this->view->message = "Page Not Found";
+                $this->renderScript('error/error_404.phtml');
                 break;
             default:
                 // application error
                 $this->getResponse()->setHttpResponseCode(500);
                 $priority = Zend_Log::CRIT;
+                $this->view->error_code = $this->getResponse()->getHttpResponseCode();
                 $this->view->message = 'Application error';
+                if ($log = $this->getLog()) {
+                    $log->log($this->view->message, $priority, $errors->exception);
+                    $log->log('Request Parameters', $priority, $errors->request->getParams());
+                    $this->renderScript('error/error.phtml');
+                }
+                if ($this->getInvokeArg('displayExceptions') == true) {
+                        $this->view->exception = $errors->exception;
+                }
+
+                $this->view->request = $errors->request;
+                $this->view->error_code = $this->getResponse()->getHttpResponseCode();
+                $this->renderScript('error/error.phtml');
                 break;
         }
         
